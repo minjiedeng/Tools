0f2ec4c6247a6b0753448fb5b4dea3fc66bd2b99
Minjie Deng <mdeng7@ur.rochester.edu>
2019-04-19 10:59:32 -0400

    Update and rename multiplication to dot.py

---
+++ dot.py
@@ -0,0 +1,17 @@
+# numpy.dot(a, b, out=None): If a is an N-D array and b is an M-D array (where M>=2), 
+# it is a sum product over the last axis of a and the second-to-last axis of b. Thus,
+# The last dimension of a must be the same size as the second-to-last dimension of b.
+
+# Example:
+import numpy as np
+
+
+# In a sovereign default model, where default_states(nA,nB)
+default_states = Vd_compat > self.Vc
+self.default_prob[:, :] = np.dot(self.PA, default_states)
+
+# If defualt_states(nA,nB,nx) with anther dimension, the following raises error:
+self.default_prob[:, :] = np.dot(self.PA, default_states) 
+
+
+
